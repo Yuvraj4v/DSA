54889a9a8afe8d28980f01b3a3503a7f235cb4c7
Yuvraj4v <ranayuvraj063@gmail.com>
2026-06-08 16:38:11 +0530

    Create Bruteforce.cpp

---
+++ HashingProblems/3SumProblem/Bruteforce.cpp
@@ -0,0 +1,38 @@
+// leetcode problem number 15
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+using namespace std;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        set<vector<int>> s;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1 ; j < n ; j++){
+                for(int k = j+1 ; k < n ; k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        vector<int> trip = {nums[i],nums[j],nums[k]};
+                        sort(trip.begin(),trip.end());
+                        if(s.find(trip) == s.end()){
+                            s.insert(trip);
+                            ans.push_back(trip);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+int main(){
+    vector<int> arr = {-1,0,1,2,-1,-4};
+    vector<vector<int>> result = threeSum(arr);
+    for(int i = 0 ; i < result.size() ; i++){
+        for(int j = 0 ; j <result[0].size() ; j++){
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
