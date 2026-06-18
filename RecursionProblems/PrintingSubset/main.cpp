3e8d26b2032fc287e4824c3b40334329ce76eaa7
Yuvraj4v <ranayuvraj063@gmail.com>
2026-06-18 18:49:49 +0530

    Create main.cpp

---
+++ RecursionProblems/PrintingSubset/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void printsubset(vector<int> &arr , vector<int> &ans , int i){
+    if(i==arr.size()){
+        for(int x : ans){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ans.push_back(arr[i]);
+    printsubset(arr,ans,i+1);
+    ans.pop_back();
+    printsubset(arr,ans,i+1);
+}
+int main(){
+    vector<int> arr = {1,2,3};
+    vector<int> ans;
+    printsubset(arr,ans,0);
+}
