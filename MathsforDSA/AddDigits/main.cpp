803f71ec804475f1151debedefd4304bfd97c355
Yuvraj4v <ranayuvraj063@gmail.com>
2026-08-13 21:37:29 +0530

    Create main.cpp

---
+++ MathsforDSA/AddDigits/main.cpp
@@ -0,0 +1,19 @@
+//leetcode problem number 258
+#include <iostream>
+using namespace std;
+
+int addDigits(int num) {
+    if (num == 0)
+        return 0;
+
+    return 1 + (num - 1) % 9;
+}
+
+int main() {
+    int num;
+    cin >> num;
+
+    cout << addDigits(num);
+
+    return 0;
+}
