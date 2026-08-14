dc842ff4dfd949966499cb2c8eb4d3c57e1a8d48
Yuvraj4v <ranayuvraj063@gmail.com>
2026-08-14 19:59:59 +0530

    Create main.cpp

---
+++ MathsforDSA/Divisible&NondivisibleSumDiffrence/main.cpp
@@ -0,0 +1,21 @@
+//leetcode problem number 2894
+#include<iostream>
+using namespace std;
+    int differenceOfSums(int n, int m) {
+        int num1 = 0;
+        int num2 = 0;
+
+        for(int i = 1 ; i <= n ; i++){
+            if(i % m == 0){
+                num2 += i;
+            }else{
+                num1 += i;
+            }
+        }
+        return num1 - num2;
+    }
+int main(){
+    int n = 10;
+    int m = 3;
+    cout<<differenceOfSums(n,m);
+}
