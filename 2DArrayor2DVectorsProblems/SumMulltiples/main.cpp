2411f8c718f461dacbf9898b6fbd6b2278e21a05
Yuvraj4v <ranayuvraj063@gmail.com>
2026-08-21 19:55:46 +0530

    Create main.cpp

---
+++ 2DArrayor2DVectorsProblems/SumMulltiples/main.cpp
@@ -0,0 +1,15 @@
+//leetcode problem number is 2652
+#include<iostream>
+using namespace std;
+    int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i = 1 ; i <= n ; i++){
+            if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0){
+                sum += i;
+            }
+        }
+        return sum;
+    }
+int main(){
+    cout<<sumOfMultiples(7);
+}
