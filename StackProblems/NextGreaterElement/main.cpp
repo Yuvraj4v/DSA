db3641ad4a73486fb35d4ca12001d6cf5dd58835
Yuvraj4v <ranayuvraj063@gmail.com>
2026-09-02 19:42:57 +0530

    solved stack problems

---
+++ StackProblems/NextGreaterElement/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+using namespace std;
+    vector<int> nextLargerElement(vector<int>& arr) {
+        // code here
+        stack<int> s;
+        vector<int> ans(arr.size(),0);
+        
+        for(int i = arr.size()-1 ; i >= 0 ; i--){
+            while(s.size() > 0 && s.top() <= arr[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = s.top();
+            }
+            s.push(arr[i]);
+        }
+        return ans; 
+    }
+int main(){
+    vector<int> arr = {6,8,0,1,3};
+    vector<int> result = nextLargerElement(arr);
+    for(int val : result){
+        cout<<val<<" ";
+    }
+}
