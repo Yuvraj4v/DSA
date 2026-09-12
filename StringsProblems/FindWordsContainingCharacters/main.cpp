03a10601793545427444edb7926cf479b7ef8ba8
Yuvraj4v <ranayuvraj063@gmail.com>
2026-09-12 20:54:52 +0530

    solve problems

---
+++ StringsProblems/FindWordsContainingCharacters/main.cpp
@@ -0,0 +1,24 @@
+//leetcode problem number is 2942
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i].find(x) != string::npos) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+int main(){
+    vector<string> arr = {"leet","code"};
+    vector<int> result = findWordsContaining(arr,'e');
+    for(int val : result){
+        cout<<val<<" ";
+    }
+}
