1de40f133938eed0089f51e002eb105e6331f42b
Yuvraj4v <ranayuvraj063@gmail.com>
2026-09-19 21:00:37 +0530

    Create main.cpp

---
+++ QueueProblems/FirstUniqueCharacterInAString/main.cpp
@@ -0,0 +1,23 @@
+//leetcode problem number is 
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<queue>
+using namespace std;
+    int firstUniqChar(string s) {
+        unordered_map<char,int> m;
+        queue<int> q;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(m.find(s[i]) == m.end()){
+                q.push(i);
+            }
+            m[s[i]]++;
+            while(q.size() > 0 && m[s[q.front()]] > 1){
+                q.pop();
+            }
+        }
+        return q.empty() ? -1 : q.front();
+    }
+int main(){
+    cout<<firstUniqChar("loveleetcode");
+}
